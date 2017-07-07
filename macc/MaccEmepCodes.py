b8902113cfeb85703634c418facac58f7c10f286
mifads <david.simpson@met.no>
2017-07-07 11:07:23 +0200

    Script to convert between MACC and EMEP iso codes

---
+++ macc/MaccEmepCodes.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+"""
+ This file sets EMEP codes, Iso3 and Iso2 (except where the EMEP
+ model uses 3-letter Iso2 - e.g. ATL!)
+"""
+import collections
+import os
+import sys
+import numpy as np
+
+# 1) Country codes file: #EMEP   ISO3   ISO2  Name #1     ALB    AL    Albania
+
+emepcodes=collections.OrderedDict() # stores iso2, iso3 etc.
+iso2toIso3=collections.OrderedDict() # stores iso2, iso3 etc.
+
+#tnonum =dict()
+CountryList="""##################################################
+# This file sets EMEP codes, Iso3 and Iso2 (except where the EMEP
+# model uses 3-letter Iso2 - e.g. ATL!)
+#EMEP   ISO3   ISO2  Name
+1     ALB    AL    Albania
+56    ARM    AM    Armenia
+2     AUT    AT    Austria
+69    AZE    AZ    Azerbaijan
+3     BEL    BE    Belgium
+4     BGR    BG    Bulgaria
+50    BIH    BA    Bosnia_and_Herzegovina
+39    BLR    BY    Belarus
+24    CHE    CH    Switzerland
+55    CYP    CY    Cyprus
+46    CZE    CZ    Czech_Republic
+60    DEU    DE    Germany
+6     DNK    DK    Denmark
+22    ESP    ES    Spain
+43    EST    EE    Estonia
+7     FIN    FI    Finland
+8     FRA    FR    France
+27    GBR    GB    United_Kingdom
+54    GEO    GE    Georgia
+11    GRC    GR    Greece
+49    HRV    HR    Croatia
+12    HUN    HU    Hungary
+14    IRL    IE    Ireland
+13    ISL    IS    Iceland
+15    ITA    IT    Italy
+53    KAZ    KZ    Kazakhstan
+68    KGZ    KG    Kyrgyzstan
+45    LTU    LT    Lithuania
+16    LUX    LU    Luxembourg
+44    LVA    LV    Latvia
+41    MDA    MD    Moldova_Republic_of
+52    MKD    MK    Macedonia
+57    MLT    MT    Malta
+17    NLD    NL    Netherlands
+18    NOR    NO    Norway
+19    POL    PL    Poland
+20    PRT    PT    Portugal
+21    ROU    RO    Romania
+61    RUS    RU    Russian_Federation
+47    SVK    SK    Slovakia
+48    SVN    SI    Slovenia
+23    SWE    SE    Sweden
+25    TUR    TR    Turkey
+40    UKR    UA    Ukraine
+51    YUG    CS    Serbia_and_Montenegro		
+30    BAS    BAS   Baltic_Sea
+31    NOS    NOS   North_Sea
+32    ATL    ATL   Atlantic_Ocean
+70    ATX    ATX   Rest_of_Atlantic_Ocean
+33    MED    MED   Mediterranean_Sea
+34    BLS    BLS   Black_Sea
+58    ASI    ASI   Rest_of_Asia
+350   INT    INTSHIPS    International_shipping
+""" #############################################################
+
+def getMaccEmepCodes():
+
+   for ncc, line in list(enumerate(CountryList.splitlines())):
+      if line == '': continue    # header
+      if line.startswith('#'): continue    # header
+      cc, iso3, iso2, name = line.split()
+      emepcodes[iso3] = dict( cc = cc, iso2=iso2, iso3=iso3, name=name )
+      iso2toIso3[iso2] = iso3
+#      tnonum[iso3] = ncc   # eg GBR -> 27
+   return emepcodes  # , iso2toIso3
+
+def getIso3(iso2):
+    """ Uses dict from above to convert emep iso2 codes to iso3 for MACC """
+    return iso2toIso3[iso2]
+
+if __name__ == '__main__':
+
+    c=getMaccEmepCodes()
