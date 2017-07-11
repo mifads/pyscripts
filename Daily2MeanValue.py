aa66b27b740ccef1a4d5f7e2f00c88fca3af8b3e
David Simpson <david.simpson@met.no>
2017-07-11 14:10:49 +0200

    Bug fixes

--- Daily2MeanValue.py
+++ Daily2MeanValue.py
@@ -12,26 +12,28 @@ def Daily2MeanValue(sday,eday,vals,dbg=False):
    concSum = 0.0
    timeSum =0.0
    
-   for day in range(d1,d2+1):  # Assumed to styart from day 0 = 1/Jan
+   for jday in range(d1,d2+1):  # Assumed to styart from day 0 = 1/Jan
+     day = min( jday, len(vals)-1 ) # to stop problems at end
      v= vals[day]
+
      if day ==  d1:
         dt= 1.0 - (sday-day)  # Often 0.25 or so, but not always
         if d2==d1: dt = 1.0   # Just use daily value
-        valThisDay = dt * v
+        if dbg: print('D2A: ', sday, eday, d1,d2, day, dt, v )
      elif day == d2 :
         dt= eday-day  # Often 0.25 or so, but not always
-        valThisDay = dt * v
+        if dbg: print('D2B: ', sday, eday, d1,d2, day, dt, v )
      else:
         dt=1.0
-        valThisDay = v
+        if dbg: print('D2C: ', sday, eday, d1,d2, day, dt, v )
    
-     concSum  += dt*valThisDay
+     concSum  += (dt*v)
      timeSum  += dt
-     if dbg: print( '%3d %7.2f %8.2f %8.3f %8.3f' % (  day, dt, 
-                      timeSum, valThisDay, vals[day] ))
+
+     if dbg: print('D2M: %3d %7.2f %8.2f %8.3f' % (  day, dt, timeSum, v ))
    
    meanVal = concSum/timeSum
-   if dbg: print(sday, eday, 'timeSum ', timeSum, 'MEAN ',  meanVal )
+   if dbg: print('D2M:',sday, eday, 'timeSum ', timeSum, 'MEAN ',  meanVal )
    return meanVal
    
 if __name__ == '__main__':
@@ -41,4 +43,6 @@ if __name__ == '__main__':
   eday = 4.29
   m1= Daily2MeanValue(sday,eday,vals,dbg=True)
   eday = 1.59
+  m2= Daily2MeanValue(sday,eday,vals,dbg=True)
+  eday = 7.0  
   m2= Daily2MeanValue(sday,eday,vals,dbg=True)
