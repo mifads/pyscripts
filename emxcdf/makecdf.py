013c273971255bcb2d59b541a72f31cbd52616f2
mifads <david.simpson@chalmers.se>
2018-03-06 09:03:43 +0100

    revised use of dictionaries

--- emxcdf/makecdf.py
+++ emxcdf/makecdf.py
@@ -21,15 +21,15 @@ def create_cdf(variables,ofile,typ,lons,lats,lonlatfmt='full',txt='',dbg=False):
   cdf.Conventions = 'CF-1.6'
   cdf.projection = 'lon lat'
   cdf.history = 'Created ' + time.ctime(time.time())
-  cdf.description = 'From emxcdf.makecdf module '+ txt
+  cdf.description = 'From emxcdf.makecdf  module '+ txt
   nx=len(lons)
   ny=len(lats)
 
   lon= cdf.createDimension('lon',nx)
   lat= cdf.createDimension('lat',ny)
-# typ can be e.g. u2, u8, f4
-# where u2 = 16 bit unsigned int, i2 = 16 bit signed int, 
-# f = f4, d = f8 
+
+# typ can be e.g. u2, u8, f4 where u2 = 16 bit unsigned int, i2 = 16 bit signed
+#  int, f = f4, d = f8 
 # CAREFUL. If file exists, or some problem, can get error
 # "Can't add HDF5 file metadata"
 # May 12th . changed longitude to lon in 1st. 2018-02-26 - added back full:
@@ -46,36 +46,27 @@ def create_cdf(variables,ofile,typ,lons,lats,lonlatfmt='full',txt='',dbg=False):
 
   if dbg :
     print('NX NY ', nx, ny, lons.max(), lats.max())
-    print('SHAPE data ', data.shape)
+    #print('SHAPE data ', data.shape)
     print('LATS', latvar )
 
   # Fill coord  data
   lonvar[:] = lons[:]
   latvar[:] = lats[:]
 
+  for var in variables.keys():
 
-  # We need a list of dictionaries. If variables is just a single
-  # dict, we make it into a list
+   print('VAR:', var)
+   print('VAR:', variables[var])
+   datvar = cdf.createVariable(var,typ ,('lat', 'lon',),zlib=True)
+   datvar[:,:] = variables[var]['data'][:,:] # fill data
 
-  if isinstance(variables, dict):
-
-    variables = [ variables, ]
-    varname=variables[0]['name']
-
-  for n, var in enumerate(variables):
-     varname=var['name']
-     datvar = cdf.createVariable(varname,typ ,('lat', 'lon',),zlib=True)
-     #if len(data.shape) == 2:
-     #  datvar[:,:] = data[:,:]
-     #else:
-     #  datvar[:,:] = data[n,:,:]
-
-     for key in var.keys():
-       if key == 'name' : continue # already done
-       if key == 'data' : 
-         datvar[:,:] = var['data'][:,:]
-         
-       datvar.setncattr(key,var[key])
+   for key in variables[var].keys():
+     print('KEY', key)
+     if key is 'data':
+       pass
+     else:
+       print('ATTR', key, variables[var][key])
+       datvar.setncattr(key,variables[var][key])
 
   print( 'NX NY VAR ', nx, ny, np.max(cdf.variables[lonv][:]),
      np.max(cdf.variables[latv][:]))
@@ -156,6 +147,7 @@ def createCDF(variables,ofile,typ,lons,lats,data,lonlatfmt='full',txt='',dbg=Fal
 
 if __name__ == '__main__':
   import matplotlib.pyplot as plt
+  from collections import OrderedDict as odict
   lons = np.linspace(-179.5,179.5,360)
   lats = np.linspace(-89.5,89.5,180)
   data = np.zeros([180,360],dtype=np.float)
@@ -169,10 +161,11 @@ if __name__ == '__main__':
   #plt.show()
 
   # 1. Example of simple scalar field
-  TestVar=dict( name='TEST1', units='uuu' )
-  createCDF(TestVar,'tmp_mkCdfm.nc','f4',lons,lats,data,dbg=True)
+  #TestVar=dict( name='TEST1', units='uuu' )
+  #createCDF(TestVar,'tmp_mkCdfm.nc','f4',lons,lats,data,dbg=True)
 
-  TestVar=dict( name='TEST1', units='uuu', data=data )
+  TestVar=odict()
+  TestVar['TEST2D'] = dict(units='uuu', data=data )
   create_cdf(TestVar,'tmp_create_cdf.nc','f4',lons,lats,data,dbg=True)
 
   # 2. Example of multiple scalar fields
@@ -182,18 +175,18 @@ if __name__ == '__main__':
   for n in range(3):
        data3[n,:,:] = data[:,:]*(n+1)
 
-  variables= [   
-     dict(name='Var1',units='ppb',long_name='test_variable with CF-illegal units'), 
-     dict(name='Var2',units='ug/m3'), 
-     dict(name='Var3',units='m s-1')
-  ]
-  createCDF(variables,'tmp_mkCdfm3.nc','f4',lons,lats,data3,
-             txt='Demo of 3 variables',dbg=False)
-  variables= [   
-     dict(name='Var1',units='ppb',long_name='test_variable with CF-illegal units',data=data3[0,:,:]), 
-     dict(name='Var2',units='ug/m3',data=data3[1,:,:]), 
-     dict(name='Var3',units='m s-1',data=data3[2,:,:])
-  ]
+  #variables= [   
+  #   dict(name='Var1',units='ppb',long_name='test_variable with CF-illegal units'), 
+  #   dict(name='Var2',units='ug/m3'), 
+  #   dict(name='Var3',units='m s-1')
+  #]
+  #createCDF(variables,'tmp_mkCdfm3.nc','f4',lons,lats,data3,
+  #           txt='Demo of 3 variables',dbg=False)
+
+  variables= odict()
+  variables['Var1']= dict(units='ppb',long_name='test_variable with CF-illegal units',data=data3[0,:,:])
+  variables['Var2']= dict(units='ug/m3',data=data3[1,:,:])
+  variables['Var3']= dict(units='m s-1',data=data3[2,:,:])
   create_cdf(variables,'tmp_create_cdf3.nc','f4',lons,lats,
              txt='Demo of 3 variables',dbg=False)
   
