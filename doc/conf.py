f4045d42c6e6f80dbaea47fa23cc33bd1dd6e05d
mifads <david.simpson@met.no>
2019-12-01 16:33:46 +0100

    classic theme

--- doc/conf.py
+++ doc/conf.py
@@ -16,6 +16,8 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+#DS adding:
+#failed import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
 
@@ -78,6 +80,7 @@ pygments_style = 'sphinx'
 # a list of builtin themes.
 #
 html_theme = 'alabaster'
+html_theme = 'classic'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
