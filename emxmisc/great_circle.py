0eb1d0b07f6b2025a2b28d1979e6b7f400bda6be
David Simpson <david.simpson@met.no>
2020-05-24 10:32:59 +0200

    Adding faster versions and longitude_distance

--- emxmisc/great_circle.py
+++ emxmisc/great_circle.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+from math import radians, sin, cos, acos, atan2, sqrt, pi
 
 """Distance helpers."""
  
@@ -8,8 +9,23 @@ import math
  
 EARTH_RAD = 6378137.0     # earth circumference in meters
  
+def longitude_distance(lat):  # 247 ns
+  """ lenght of 1 degree arc along latitude lat, in m """
+  rad = EARTH_RAD*cos(radians(lat))
+  dist = 2*pi*rad/360  # 111 km when rad = R
+  return dist
+
+def great_circle_distance2(slat,elat,slon,elon):  # 626 ns
+  """ returns distance in km """
+  slat= radians(slat)
+  slon= radians(slon)
+  elat= radians(elat)
+  elon= radians(elon)
+  dist =  EARTH_RAD * acos(sin(slat)*sin(elat) + \
+              cos(slat)*cos(elat)*cos(slon - elon))
+  return dist
  
-def great_circle_distance(latlong_a, latlong_b):
+def great_circle_distance(latlong_a, latlong_b):  # 1.0 us
     """
     >>> coord_pairs = [
     ...     # between eighth and 31st and eighth and 30th
@@ -32,19 +48,23 @@ def great_circle_distance(latlong_a, latlong_b):
     lat1, lon1 = latlong_a
     lat2, lon2 = latlong_b
  
-    dLat = math.radians(lat2 - lat1)
-    dLon = math.radians(lon2 - lon1)
-    a = (math.sin(dLat / 2) * math.sin(dLat / 2) +
-            math.cos(math.radians(lat1)) * math.cos(math.radians(lat2)) * 
-            math.sin(dLon / 2) * math.sin(dLon / 2))
-    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    dLat = radians(lat2 - lat1)
+    dLon = radians(lon2 - lon1)
+    a = (sin(dLat / 2) * sin(dLat / 2) +
+            cos(radians(lat1)) * cos(radians(lat2)) * 
+            sin(dLon / 2) * sin(dLon / 2))
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
     d = EARTH_RAD * c
     
     return d
 
 if ( __name__ == '__main__' ):
   print("1deg@60N EW", great_circle_distance(latlong_a=( 60.0,0.0), latlong_b=( 60.0,1.0)))
+  print("simp@60N EW", great_circle_distance2(60.0,60.0,0.0,1.0))
+  print("arc @60N EW", longitude_distance(60.0))
   print("1deg@60N NS", great_circle_distance(latlong_a=( 60.0,0.0), latlong_b=( 61.0,0.0)))
   print("1deg 45EW", great_circle_distance(latlong_a=( 45.0,0.0), latlong_b=( 45.0,1.0)))
+  print("simp@45N EW", great_circle_distance2(45.0,45.0,0.0,1.0))
+  print("arc @45N EW", longitude_distance(45.0))
   print("1deg@45 NS", great_circle_distance(latlong_a=( 45.0,0.0), latlong_b=( 46.0,0.0)))
 
