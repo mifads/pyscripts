6b7fc9e81a8eb65a3a06fc1a828f350046d3bc8b
David Simpson <david.simpson@met.no>
2020-05-28 14:48:33 +0200

    Much improved looping

--- pyRef.py
+++ pyRef.py
@@ -27,28 +27,16 @@ testr='Documents/TeX/testr.bib'
 testr='Documents/TeX/fixed.testr.bib'  # fixtext2
 testr=homedir+'Documents/TeX/Refs.bib'
 
-test_export = True
-with open(testr,encoding='utf-8') as bfile:
-#with open(testr,encoding='latin-1') as bfile:
-   bb=bibtexparser.load(bfile, parser)
+test_export = False # True
 
-   if test_export: # Keeps acp now:
-     a= bb.entries_dict['Yttri:Urban']
-     db=BibDatabase()
-     db.entries = [a]
-     writer=BibTexWriter()
-     with open('testoutbib.bib','w') as bibfile:
-       bibfile.write(writer.write(db))
-     sys.exit()
-
-   #sys.exit()
+def query_user():
    search_terms = input('Give search terms:') # eg QQDEP, 2014
    search_terms = search_terms.split()
    print(search_terms)
    print(len(bb.entries_dict)) # 6297
-   #print(bb.entries_dict ) -> prints lots! keys
+   return search_terms
 
-   npdf = 0
+def scan_for_pdfs(search_terms):
    pdfs = []
    for key in bb.entries_dict:
      entry = bb.entries_dict[key]
@@ -57,36 +45,75 @@ with open(testr,encoding='utf-8') as bfile:
 
      if all ( x in str(copy) for x in search_terms):
        if 'file' in entry:
-         print('MATCH:', npdf, key, copy['file']) 
+         print('MATCH:', len(pdfs), key, copy['file']) 
          pdf = copy['file'].split(':')[1]
          if pdf.startswith('/home/'):
            pdfs.append( pdf )
          else:
            pdfs.append( texdir + pdf )
-         npdf += 1
        else:
-         print('MATCH:', key )
-   if npdf > 0:
-      for view in range(10):
-        ipdf = input('Open pdf? Give number (or type x to exit): ')
-        print('IPDF:%s:' % ipdf )
-        if ipdf == 'x':
-          print('XPDF:%s:' % ipdf )
-          #sys.exit()
-          break
-        else:
-          print('NPDF:%s:' % ipdf )
-          print('Opens pdf number', ipdf)
-          subprocess.call('evince %s' %  pdfs[int(ipdf)],shell=True)
-   sys.exit()
-      
+         print('NO PDF:', key )
+   return pdfs
 
-   #print(bb.strings)  # e.g. acp = 
+def process_pdfs(pdfs):
+
+   retcode = 'X'
+   if len(pdfs) > 0:
+      while True:
+        ans = input('Open pdf? Give number, type n for new search, x to exit: ')
+
+        if ans == 'x':
+          sys.exit('Exit called')
+        elif ans =='n':
+          retcode = 'N'
+          break
+        elif ans.isalpha(): 
+          print('Illegal answer: %s !! Need n, x or number. Try again' % ans)
+          continue
+        else:
+          ipdf = int(ans)
+          if ipdf >= len(pdfs):
+            print('NUMBER TOO HIGH: %s !! Try again' % ans)
+            continue
+          else:
+            print('Opens pdf number', ipdf)
+            subprocess.call('evince %s' %  pdfs[int(ipdf)],shell=True)
+            retcode = 'C'
+   return retcode
+
+def test_bibexport():
+     a= bb.entries_dict['Yttri:Urban']
+     db=BibDatabase()
+     db.entries = [a]
+     writer=BibTexWriter()
+     with open('testoutbib.bib','w') as bibfile:
+       bibfile.write(writer.write(db))
+     sys.exit()
+
+with open(testr,encoding='utf-8') as bfile:
+#with open(testr,encoding='latin-1') as bfile:
+   bb=bibtexparser.load(bfile, parser)
+
+   if test_export: test_bibexport()
+
+   while True:
+     search_terms = query_user()
+
+     pdfs = scan_for_pdfs(search_terms)
+
+     p = process_pdfs(pdfs)
+     if p == 'N':
+        continue # New search
+     else:
+        sys.exit('Finished')
+      
+sys.exit('OOPS Shouldnt get here')
+
+#print(bb.strings)  # e.g. acp = 
 #   print(bb.entries_dict['Gon2013'] )
-   g= bb.entries_dict['Gon2013']
-   a= bb.entries_dict['Yttri:Urban']
+#   g= bb.entries_dict['Gon2013']
+#   a= bb.entries_dict['Yttri:Urban']
 #   print('doi: ' , g['doi'])
-sys.exit()
 
 #with open('DFILE.bib','w') as dfile:
 #  bibtexparser.dump(bb, dfile)
