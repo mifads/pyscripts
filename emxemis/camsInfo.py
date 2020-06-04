c6c12c73c83e4696218c3c5ac3ae06db017674e8
David Simpson <david.simpson@met.no>
2020-06-04 09:05:49 +0200

    More outputs, incl snap2 and PM

--- emxemis/camsInfo.py
+++ emxemis/camsInfo.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python3
 """
-  Reads TNO MACC format emission file and stores as camsInfo
-  giving e.g. camsInfo['Emis:LUX']['nox']['A2:P']
-  Updated Nov 2019 for new sector possibilities, and to use pandas
+  Reads TNO MACC/CAMS format emission file and stores as dictionary giving
+  e.g.:
+    Emis['NH3']['NOR']['C:A']['sum'] for sum of GNFR sector C (:A = area)
+    Emis['snap2']['PMc']['NOR']] for small combustion (=special!)
+  or Emis['NOX']['NOR']['A:P']['vals'][:,:] for 2-D mapping of that sector
+
+  Updated Nov 2019 - Apr 2020 for new sector possibilities, and to use pandas
   previously maccInfo:  July 2017
+
+  Warning 1. Too complex, and not yet re-working for older MACC style, which
+  had irregular lon/lat coords. Easily fixed, but not done. Works for new
+  CAMS though.
+
+  Warning 2. Uses lots of memory and takes time!
 """
 from collections import OrderedDict as odict
 import copy
@@ -23,7 +33,7 @@ Usage="""
 
 camsInfo = odict()
 
-# 2)  Emissions
+# Emissions files, e.g.:
 # TNO emissions 2017-ish:
 #Lon;Lat;ISO3;Year;SNAP;SourceType;CH4;CO;NH3;NMVOC;NOX;PM10;PM2_5;SO2
 #-29.937500;36.406250;ATL;2011;8;A;0.000000;0.063392;0.000000;0.018761;0.634203;0.048957;0.046509;0.417624
@@ -60,13 +70,16 @@ def check_ranges(coords,txt):
   dcoord= coords[1]-coords[0] # Hopefully a good dx, dy guess
   print( txt, ' coords', nicefloats([ dcoord, coords[0], coords[-1], 
      coords[-1]-coords[0], (coords[-1]-coords[0])/dcoord, dmin, dmax ] ) )
+  assert abs(dmax-dmin) < 1.0e-9,'IRREGULAR COORDS %s %f %f %12.3e' % (txt, dmin, dmax, dmax-dmin)
+#  assert dmax>dmin,'AIRREGULATE COORDS %s %f %f' % (txt, dmin, dmax)
+#  assert dmax<dmin,'BIRREGULATE COORDS %s %f %f %f' % (txt, dmin, dmax, dmax-dmin)
 
-def readCams(ifile,wanted_poll=None,get_vals=False,dbgcc=None):
 
+def readCams(ifile,wanted_poll=None,get_vals=False,dbgcc=None,dbg=None):
 
     print('Reading %s;\n *** can take a while!' % ifile )
     df = pd.read_csv(ifile,sep=';')
-    df.info(memory_usage='deep')
+    if dbg: df.info(memory_usage='deep')
     
     used_polls = polls
     if wanted_poll == 'PMc' or wanted_poll == 'PM':
@@ -84,10 +97,9 @@ def readCams(ifile,wanted_poll=None,get_vals=False,dbgcc=None):
     print('KEYS', wanted_poll, df.keys() )
     #df.info(memory_usage='deep')
     #df.to_sparse().info(memory_usage='deep')
-    #sys.exit()
 
     # 1-d fields:
-    lonList    = df.iloc[:,0].values
+    lonList    = df.iloc[:,0].values  # Name can change between MACC/CAMS
     latList    = df.iloc[:,1].values
     iso3List   = df.ISO3.values
     sec_key    = df.keys()[4]   # eg GNFR_Sector or SNAP
@@ -95,18 +107,20 @@ def readCams(ifile,wanted_poll=None,get_vals=False,dbgcc=None):
     secList    = df[sec_key].values
     typList    = df.SourceType.values
     iso3s = np.unique(iso3List)
-    iso3s = np.append(iso3s,'Total')
-    if dbgcc is not None: print(iso3s)
+    EurTot = 'EurTot'  # sum of all countries
+    iso3s = np.append(iso3s,EurTot)
+    if dbgcc is not None: print('DBGISO3s: ',iso3s)
+    if dbg   is not None: print('DBGISO3s: ',iso3s)
     
     sectors = sorted( df[sec_key].unique() )
     types   = sorted( df[typ_key].unique() )
-    ccTot = 'Sum'
+    countryTot = 'Sum:secs'  # Sum for each country
     srcs = []
     for sec, typ in zip( secList, typList ):
        srcs.append( src_name( sec_key, sec, typ) )
 
     srcs = np.unique(srcs)  #  np unique also sorts
-    srcs = np.append(srcs,ccTot) # at end
+    srcs = np.append(srcs,countryTot) # at end
     print('Zipped list from sec_key, typ_key', srcs, len(srcs) )
 
    # Find lon/lat ranges and dimensions
@@ -137,6 +151,9 @@ def readCams(ifile,wanted_poll=None,get_vals=False,dbgcc=None):
 
     srcEmis =  dict()
     srcEmis['polls']=used_polls.copy()
+    srcEmis['iso3s']= iso3s
+    srcEmis['srcs'] = srcs
+    srcEmis['snap2']= dict() # special
     srcEmis['lons']=lons.copy()
     srcEmis['lats']=lats.copy()
     srcEmis['dx']=dx
@@ -150,8 +167,11 @@ def readCams(ifile,wanted_poll=None,get_vals=False,dbgcc=None):
 
        # Initialise dict()
        srcEmis[poll]  = dict()
+       if poll.startswith('PM'): srcEmis['snap2'][poll] = dict()
        for iso3 in iso3s:
          srcEmis[poll][iso3] = dict()
+         if poll.startswith('PM'): 
+           srcEmis['snap2'][poll][iso3] = 0.0 # special
          for src in srcs:
            srcEmis[poll][iso3][src] = dict()
            srcEmis[poll][iso3][src]['sum']  =  0.0
@@ -179,12 +199,15 @@ def readCams(ifile,wanted_poll=None,get_vals=False,dbgcc=None):
           x =  vals[n]
 
           srcEmis[poll][iso3][src]['sum']    += x
-          srcEmis[poll][iso3][ccTot]['sum']  += x
+          srcEmis[poll][iso3][countryTot]['sum']  += x
+          srcEmis[poll][EurTot][src]['sum']  += x
+          srcEmis[poll][EurTot][countryTot]['sum']  += x
+          if src=='C:A' and poll.startswith('PM'):
+                srcEmis['snap2'][poll][iso3] += x
           #print('GET VALS?', get_vals)
           #MAR2020 if get_vals is not None:
           if get_vals:
              srcEmis[poll][iso3][src]['vals'][iy,ix] += x
-    
      
 #          if ix == idbg and iy==jdbg: 
 #            print('DBG ', poll, lon, lat, src, x, srcEmis[src][iso3][iy,ix] )
@@ -231,24 +254,19 @@ if __name__ == '__main__':
 
   print('IFILE', ifile)
 
-  m=readCams(ifile,wanted_poll='PM',get_vals=False,dbgcc='NOR')  #PMc is special
-  gc.collect() # recover some memory?
+  # ==========================================================
 
-  print('EXAMPLE gnfr:', m['PMc']['NOR']['C:A']['sum'])
+  dbgcc='POL'
+  m=readCams(ifile,wanted_poll='PM',get_vals=False,dbgcc=dbgcc)  #PMc is special
+
+  print('EXAMPLE gnfr:', m['PMc'][dbgcc]['C:A']['sum'])
+  print('EXAMPLE snap2:', m['snap2']['PM2_5'][dbgcc] )
+  # ==========================================================
+
+  gc.collect() # recover some memory?
 
   # here we get spatial values. Seems to work, but can be memory problems
   #v=readCams(ifile,wanted_poll='PM',get_vals=True,dbgcc='Total')  #PMc is special
   #v=readCams(ifile,get_vals=True,dbgcc='Total')  #PMc is special
-
-  #m=readCams(ifile,wanted_poll='NOX',get_vals=True,dbgcc='Total')  #PMc is special
-    #df.info(memory_usage='deep')
-  #poll='NOX'; iso3='FRA'; src= 'F1:A'
-  #m=readCams(ifile,dbgcc='Total')
-  #print(np.mean( m[poll][iso3][src]['vals'][:,:] ))
-
-#  print(m['file'])
-#  print(m['domain'])
-#  print(m['Emis:FRA'][poll])
-#  print('France :\n',m['Sum:FRA'][poll])
 
 
