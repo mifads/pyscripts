e03b33ef354ce8c45bb06cab3cc4a3b6f9387e0f
David Simpson <david.simpson@met.no>
2020-06-04 09:04:32 +0200

    Adding tnoAPmapping

--- emxemis/camsSectorTables.py
+++ emxemis/camsSectorTables.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
-# The PM_split files have A,B,C,D,E,F1,F2,F3,F4,G,H,I,J,K,L,
-# NB if we split A and F, set zero emissions for those sectors
+"""
+ 1. cams3p1_table:
+ The PM_split files have A,B,C,D,E,F1,F2,F3,F4,G,H,I,J,K,L,
+ NB if we split A and F, set zero emissions for those sectors
+
+ 2. tnoRefSources
+ A conventient list of what's included in TNO's Ref1 and Ref2
+"""
 
 cams3p1_table = """# Nov 2019
 # emep  cams_code  cams_long
@@ -33,12 +39,38 @@ def getCams2emep(table=cams3p1_table,dbg=False):
     a, b, c = line.split()
     cams2emepcode[ b ] = int(a)
     cams2name[ b ] = c
-    #if dbg: print(a, cams2emep[b], c )
     if dbg: print( '\t'.join( [ b,  a, c ] ) )
   return cams2emepcode, cams2name
 
-#mapping, names =  getCams2emep(cams3p1_table,dbg=True)
+def camstab(table=cams3p1_table,dbg=False):
+  cams=dict()
+  cams['emepsec'] = []
+  cams['name']    = []
+  cams['tnosec']  = []
+  for line in table.splitlines():
+    if line.startswith('#'): continue
+    a, b, c = line.split()
+    cams['emepsec'].append(int(a))
+    cams['tnosec'].append( b)
+    cams['name'].append(c)
+  return cams
+
+def tnoAPmapping():
+  """ not used so far, but a conventient list of what's included
+      in TNO's Ref1 and Ref2 """
+  tnoAP= ['A:A', 'A:P', 'B:A', 'B:P', 'C:A', 'D:A', 'D:P', 'E:A', 'F1:A',
+       'F2:A', 'F3:A', 'F4:A', 'G:A', 'H:A', 'H:P', 'I:A', 'J:A', 'J:P',
+       'K:A', 'L:A', 'L:P']
+  # 2020 - uses A1, A2 or A, F1... or F. No use of :P for H or J
+  tno19= ['A1', 'A2', 'B', 'B', 'C', 'D', 'D', 'E', 'F1',
+       'F2', 'F3', 'F4', 'G', 'H', 'H', 'I', 'J', 'J',
+       'K', 'L', 'L']
+  return dict(zip(tnoAP, tno19))
+
 if __name__ == '__main__':
 
   mapping, names =  getCams2emep(cams3p1_table,dbg=True)
   print('SHIPPING? ', mapping['G'], names['G'] )
+  tnoAP_to_19 = tnoAPmapping()
+  ap= tnoAP_to_19['A:P'] # Returns A2
+  print('A:P? ', ap, mapping[ap], names[ap] )
