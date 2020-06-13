1a54bbcb9b492b6b8b7fe0d77d385418795e866c
--get <david.simpson@met.no>
2020-06-13 13:22:03 +0200

    Added snap column in table

--- emxemis/camsSectorTables.py
+++ emxemis/camsSectorTables.py
@@ -9,36 +9,38 @@
 """
 
 cams3p1_table = """# Nov 2019
-# emep  cams_code  cams_long
-  1        A     A_PublicPower
-  2        B     B_Industry
-  3        C     C_OtherStationaryComb
-  4        D     D_Fugitive
-  5        E     E_Solvents
-  6        F     F_RoadTransport
-  7        G     G_Shipping
-  8        H     H_Aviation
-  9        I     I_Offroad
-  10       J     J_Waste
-  11       K     K_AgriLivestock
-  12       L     L_AgriOther
-  13       M     M_Other
-  14      A1     A1_PublicPower_Point
-  15      A2     A2_PublicPower_Area
-  16      F1     F1_RoadTransportExhaustGasoline
-  17      F2     F2_RoadTransportExhaustDiesel
-  18      F3     F3_RoadTransportExhaustLPGgas
-  19      F4     F4_RoadTransportNonExhaust
+# emep  cams_code  snap  cams_long
+  1        A          1  A_PublicPower
+  2        B          3  B_Industry
+  3        C          2  C_OtherStationaryComb
+  4        D          5  D_Fugitive
+  5        E          6  E_Solvents
+  6        F          7  F_RoadTransport
+  7        G          8  G_Shipping
+  8        H          8  H_Aviation
+  9        I          8  I_Offroad
+  10       J          9  J_Waste
+  11       K         10  K_AgriLivestock
+  12       L         10  L_AgriOther
+  13       M          3  M_Other
+  14      A1          1  A1_PublicPower_Point
+  15      A2          1  A2_PublicPower_Area
+  16      F1          7  F1_RoadTransportExhaustGasoline
+  17      F2          7  F2_RoadTransportExhaustDiesel
+  18      F3          7  F3_RoadTransportExhaustLPGgas
+  19      F4          7  F4_RoadTransportNonExhaust
 """
 
 def getCams2emep(table=cams3p1_table,dbg=False):
   cams2emepcode = dict()
+  cams2snapcode = dict()
   cams2name     = dict()
   for line in table.splitlines():
     if line.startswith('#'): continue
-    a, b, c = line.split()
+    a, b, c, d = line.split()
     cams2emepcode[ b ] = int(a)
-    cams2name[ b ] = c
+    cams2snapcode[ b ] = int(c)
+    cams2name[ b ] = d
     if dbg: print( '\t'.join( [ b,  a, c ] ) )
   return cams2emepcode, cams2name
 
@@ -47,12 +49,14 @@ def camstab(table=cams3p1_table,dbg=False):
   cams['emepsec'] = []
   cams['name']    = []
   cams['tnosec']  = []
+  cams['snap']  = []
   for line in table.splitlines():
     if line.startswith('#'): continue
-    a, b, c = line.split()
+    a, b, c, d = line.split()
     cams['emepsec'].append(int(a))
     cams['tnosec'].append( b)
-    cams['name'].append(c)
+    cams['snap'].append( c)
+    cams['name'].append(d)
   return cams
 
 def tnoAPmapping():
@@ -74,3 +78,5 @@ if __name__ == '__main__':
   tnoAP_to_19 = tnoAPmapping()
   ap= tnoAP_to_19['A:P'] # Returns A2
   print('A:P? ', ap, mapping[ap], names[ap] )
+
+  ctab = camstab()
