f1419ab3d0e9bf5a6df90f34f2b2cc1d812e195c
--get <david.simpson@met.no>
2020-06-23 11:24:13 +0200

    Changed to read gitted table

--- emxverify/readNiluStations.py
+++ emxverify/readNiluStations.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python3
 """
-  Reads stations list produced in June 2020, and returns
-  dict of sites data """
-import pandas as pd
-import sys
-#AT0030R Pillersdorf bei Retz                     48°43'16"N 015°56'32"E  315.0m
+  Reads stations list produced in June 2020, which starts 
 
-dataDir='/home/davids/Data/NILU/'
+   Code    StationName   ..       latitude   longitude   alt
+   AM0001R Amberd        ..       40°23'04"N 044°15'38"E 2080.0m
+   ..
+  (stored in this directory) and returns dict of sites data """
+import os.path as op
+import sys
 
 def read_nilu_sites():
+
   sites=dict()
-  f=open(dataDir + 'stations_jun2020.txt','r')
+
+  scriptDir= op.dirname( op.realpath(__file__)) # location of this script
+  f = open( scriptDir + '/stations_jun2020.txt', 'r')
+
   lines=f.readlines()
   f.close()
   for n, line in enumerate(lines):
@@ -27,3 +32,9 @@ def read_nilu_sites():
      sites[code]['name']= station
      sites[code]['alt']= float(alt)
   return sites
+
+if __name__ == '__main__':
+
+  sites = read_nilu_sites()
+  print('sites["NO01"]: ', sites['NO01'])
+  
