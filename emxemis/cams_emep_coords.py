66cb9941b91ba59c769b698782a448f49d55eec5
--get <david.simpson@met.no>
2020-09-24 11:06:09 +0200

    More roll methods. Clearer

--- emxemis/cams_emep_coords.py
+++ emxemis/cams_emep_coords.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python3
 """ collection of methods to get grid coords. Ranges from simple
     use of np-linspace to reading from a file.
+    Also has rolls to convert EMEP to CAMS or vice-vers
+
+     def getCamsGlobal05coords(dbg=False):
+     def getEmepGlobal05coords(dbg=False):
+     def getemepgrid(emepfile):
+     def getGridInfo(lats,lons):
+     def getEmepGlobal05grid():
+     def getCamsGlobal05grid():
+     def rollRight3D(camsstyle,dbg=False):
+     def rollLeft3D(emepstyle,dbg=False):
 """
 import numpy as np
 
@@ -25,7 +35,7 @@ def getemepgrid(emepfile):
        emep lons: -179.25 .. 180.25  URGH
        emep lats: -89.75 .. 89.75
   """
-
+  import netCDF4
   grid=netCDF4.Dataset(emepfile)
   lats=grid.variables['lat'][:]
   lons=grid.variables['lon'][:]
@@ -44,9 +54,50 @@ def getGridInfo(lats,lons):
       dlon=dlon, dlat=dlat, lon0edge=lon0edge, lat0edge = lat0edge )
   return gridout
 
+def getEmepGlobal05grid():
+  xlats, xlons = getEmepGlobal05coords(dbg=True)
+  return getGridInfo(xlats, xlons)
 
-if __name__ == '__main__':
+def getCamsGlobal05grid():
   xlats, xlons = getCamsGlobal05coords(dbg=True)
+  return getGridInfo(xlats, xlons)
+
+def rollRight2D(camsstyle,dbg=False):
+   """ converts EMEP-style (-179.25) to CAMS-style (-179.75)"""
+   c=camsstyle.copy()  # emepstyle
+   return np.roll(c,+1,axis=1)
+
+def rollRight3D(camsstyle,dbg=False):
+   """ converts EMEP-style (-179.25) to CAMS-style (-179.75)"""
+   c=camsstyle.copy()  # emepstyle
+   return np.roll(c,+1,axis=2)
+
+def rollLeft3D(emepstyle,dbg=False):
+   """ converts CAMS-style (-179.75) to EMEP-style (-179.25)"""
+   c=emepstyle.copy()  # camsstyle
+   return np.roll(c,-1,axis=2)
+  
+if __name__ == '__main__':
+
+  xlats, xlons = getCamsGlobal05coords(dbg=True)
+  grid=getGridInfo(xlats, xlons)
+  print('CAMS GRID', grid['lons'][0] )
   xlats, xlons = getEmepGlobal05coords(dbg=True)
   grid=getGridInfo(xlats, xlons)
-  print(grid)
+  print('EMEP GRID', grid['lons'][0] )
+
+  e=np.ones([12,4,10]) # fake emep lat,lon
+  f=np.ones([4,10]) # fake emep lat,lon
+  mm=2
+  e[:,2,1]  = 111 # need to shift longitude right
+  e[:,2,-1] = 999 # need to shift longitude right
+  f[2,1]  = 111 # need to shift longitude right
+  f[2,-1] = 999 # need to shift longitude right
+  print('RROLL pre ', e[mm,2,0], e[mm,2,1], e[mm,2,2], '..', e[mm,2,-2], e[mm,2,-1] )
+  e = rollRight3D(e)
+  print('RROLL pos ', e[mm,2,0], e[mm,2,1], e[mm,2,2], '..', e[mm,2,-2], e[mm,2,-1] )
+  e = rollLeft3D(e)
+  print('LROLL pos ', e[mm,2,0], e[mm,2,1], e[mm,2,2], '..', e[mm,2,-2], e[mm,2,-1] )
+  print('RROLL 2D pre ', f[2,0], f[2,1], f[2,2], '..', f[2,-2], f[2,-1] )
+  f = rollRight2D(f)
+  print('RROLL 2D pos ', f[2,0], f[2,1], f[2,2], '..', f[2,-2], f[2,-1] )
