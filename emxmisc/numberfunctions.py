eb4b19c455b3bfc7a950d0932c1da4cbead5ee7a
David Simpson <david.simpson@met.no>
2022-02-08 17:17:40 +0100

    Added nicefloat

--- emxmisc/numberfunctions.py
+++ emxmisc/numberfunctions.py
@@ -12,7 +12,7 @@ def nint(x):
       return int(round(x))
 
 def intround(n, p):
-   ''' rounds an intger. if "p"<0, p is a exponent of 10; if p>0, left to
+   ''' rounds an integer. if "p"<0, p is a exponent of 10; if p>0, left to
        right digits. Code from
        https://stackoverflow.com/questions/3348825/how-to-round-integers-in-python'''
    if p==0: return n
@@ -20,6 +20,12 @@ def intround(n, p):
        ln=len(str(n))  
        p=p-ln+1 if n<0 else p-ln
    return (n + 5 * 10**(-p-1)) // 10**-p * 10**-p
+
+def nicefloat(x):
+  """ Used with lon/lat ranges and dimensions
+      converts eg 0.09999999999787 to 0.1 """
+  return float('%12.6f' % x)
+
 
 if __name__ == '__main__':
   tgt=5555555
@@ -30,3 +36,8 @@ if __name__ == '__main__':
   d=2
   print('\t{} rounded to {} places:\n\t{} right to left \n\t{} left to right'.format( tgt,d,intround(tgt,-d), intround(tgt,d))) 
 
+  mytest = 130.99999999999994
+  print( nint(100*mytest) )
+  print( intround(int(100*mytest),3) )
+  print( nicefloat(mytest) )
+  print( nicefloat(-mytest) )
