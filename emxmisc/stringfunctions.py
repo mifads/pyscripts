b48f225483c71f0cc5f41813f82eec2f55f7aab8
David Simpson <david.simpson@met.no>
2024-10-18 12:39:08 +0200

    adding sep option to multiwrite

--- emxmisc/stringfunctions.py
+++ emxmisc/stringfunctions.py
@@ -18,12 +18,12 @@ def blankRemove(str):
   return ''.join( c for c in nstr)
 
 #---------------------------------------------------------------------------- 
-def multiwrite(a,fmt):
- """ formats an array into a text string, with new f format using tips from
-      https://realpython.com/how-to-python-f-string-format-float """
+def multiwrite(a,fmt,sep=''):
+  """ formats an array into a text string, with new f format using tips from
+      https://realpython.com/how-to-python-f-string-format-float
+  """
   fmt = fmt.replace('%','') # from old style to new f format
-  return "".join( f'{num:{fmt}}' for num in a )
-#OLD  return "".join( fmt%num for num in a )
+  return sep.join( f'{num:{fmt}}' for num in a )
 
 #---------------------------------------------------------------------------- 
 if __name__ == '__main__':
@@ -34,3 +34,5 @@ if __name__ == '__main__':
   a = [ 1.2, 2.3, 4.5, 6.7 ]
   print( 'MultiWrite: ', multiwrite(a,'%7.2f') )
   print( 'MultiWrite: ', multiwrite(a[0:3],'%7.2f') )
+  print( 'MultiWrite: ', multiwrite(a[0:3],'%7.2f',sep=',') )
+  #OLD print( 'MultiWrite: ', multiwrite(a,', %7.2f') )
