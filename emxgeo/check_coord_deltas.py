7e8d57435e4e5f24924414f18af47c9064251c28
David Simpson <david.simpson@met.no>
2024-11-30 09:50:20 +0100

    check_coord_deltas

---
+++ emxgeo/check_coord_deltas.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" checks for uniform coord deltas """
+
+def check_coord_deltas(coords,stop_if_variable=True):
+  dc = [ coords[n] - coords[n-1] for n in range(1,len(coords)) ]
+  maxdc = max(dc)
+  mindc = min(dc)
+  ok = 0.9999 <  maxdc/mindc < 1.0001
+  assert ok,f'VARIABLE coords! {maxdc} {mindc}'
+  return np.mean(dc) #, mindc, maxdc
+
