78e98c0c624ff57284ddb4b23f61a496658d08c5
David Simpson <david.simpson@met.no>
2024-12-19 12:17:30 +0100

    Added tol to check_coord_deltas

--- emxgeo/check_coord_deltas.py
+++ emxgeo/check_coord_deltas.py
@@ -2,11 +2,12 @@
 import numpy as np
 """ checks for uniform coord deltas """
 
-def check_coord_deltas(coords,stop_if_variable=True):
+def check_coord_deltas(coords,stop_if_variable=True,tol=0.0001):
   dc = [ coords[n] - coords[n-1] for n in range(1,len(coords)) ]
   maxdc = max(dc)
   mindc = min(dc)
-  ok = 0.9999 <  maxdc/mindc < 1.0001
+  #ok = 0.9999 <  maxdc/mindc < 1.0001
+  ok = 1-tol <  maxdc/mindc <  1+tol
   assert ok,f'VARIABLE coords! {maxdc} {mindc}'
   return np.mean(dc) #, mindc, maxdc
 
