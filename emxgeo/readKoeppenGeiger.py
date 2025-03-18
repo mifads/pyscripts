bf0bb8a06afe249cff29e4aacc280dd2513975d5
David Simpson <david.simpson@met.no>
2025-03-18 13:06:36 +0100

    Added Beck access

--- emxgeo/readKoeppenGeiger.py
+++ emxgeo/readKoeppenGeiger.py
@@ -5,7 +5,6 @@ import numpy as np
 import os
 import sys
 home=os.environ['HOME']
-ifile= home + '/Work/LANDUSE/KoeppenGeiger/Koeppen-Geiger-ASCII.txt'
 
 # Mar 2019, not used (yet)
 #KG_boreal = 1
@@ -14,7 +13,36 @@ ifile= home + '/Work/LANDUSE/KoeppenGeiger/Koeppen-Geiger-ASCII.txt'
 #KG_tropical = 4
 #KG_list = []
 
-def getKoeppenGeiger(ifile=ifile,groupsWanted=[],plotsWanted=False):
+
+def getKoeppenGeigerBeck(tdir,groupsWanted=[],plotsWanted=False,dbg=False):
+
+  import xarray as xr
+  import emxgeo.check_coord_deltas as crd
+  ds=xr.open_dataset(f'{tdir}/Beck2023/Beck2023_0p5fracs_from_0p1.nc')
+
+  kgb = dict()
+  kgb['lons'] = ds.lon.values
+  kgb['lats'] = ds.lat.values
+  dx = crd.check_coord_deltas(kgb['lons'])
+  dy = crd.check_coord_deltas(kgb['lats'])
+  lat0=kgb['lats'][0]-0.5*dy
+  lon0=kgb['lons'][0]-0.5*dx
+  print(f'KGLONS {lon0} {kgb['lons'][0]} {kgb['lons'][-1]} {dx} {len(kgb['lons'])}')
+  print(f'KGLATS {lat0} {kgb['lats'][0]} {kgb['lats'][-1]} {dy} {len(kgb['lats'])}')
+  for var in ds.keys():
+    kgb[var]= 100 * ds[var].values[:,:]
+    if dbg: print('K-G', var, np.max(kgvals[var]) )  # max 1.0
+
+  kgb['grid_desert']  = kgb['BWh_Arid_desert_hot'] + kgb['BWk_Arid_desert_cold']
+  kgb['grid_tropics'] = kgb['Af_Tropical_rainforest'] + kgb['Am_Tropical_monsoon']
+  kgb['grid_boreal']  = kgb['Dfc_Cold_no_dry_season_cold_summer'] + kgb['Dfd_Cold_no_dry_season_very_cold_winter']
+  kgb['grid_tundra']  = kgb['EF_Polar_frost'] + kgb['ET_Polar_tundra']
+  ds.close()
+
+  return kgb
+
+
+def getKoeppenGeigerOrig(groupsWanted=[],plotsWanted=False):
    """ File has:
       Lat      Lon      Cls
    -89.75  -179.75       EF
@@ -24,6 +52,7 @@ def getKoeppenGeiger(ifile=ifile,groupsWanted=[],plotsWanted=False):
    By default returns all 29 flags (Af, A...)
    but can ask for just main, e.g. [A, B ]
    """
+   ifile= home + '/Work/LANDUSE/KoeppenGeiger/Koeppen-Geiger-ASCII.txt'
    x=np.genfromtxt(ifile,names=True,dtype=None)
    xlat=x['Lat']
    xlon=x['Lon']
@@ -88,8 +117,22 @@ def getKoeppenGeiger(ifile=ifile,groupsWanted=[],plotsWanted=False):
 
 if __name__ == '__main__':
 
-   import emxcdf.makecdf as emx # Creates cdf file for a simple lonlat projection
-   lons, lats, kgdata = getKoeppenGeiger()
-   emx.create_cdf(kgdata,'Koeppen-Geiger-Mar2019.nc','i4',lons,lats,dbg=False)
-   # just test emx.create_cdf(kgdata,'Koeppen-Geiger-Aug2020.nc','i4',lons,lats,dbg=False)
-#   print('KG ', KG_boreal)
+#   import emxcdf.makecdf as emx # Creates cdf file for a simple lonlat projection
+#   lons, lats, kgdata = getKoeppenGeiger()
+#   emx.create_cdf(kgdata,'Koeppen-Geiger-Mar2019.nc','i4',lons,lats,dbg=False)
+#   # just test emx.create_cdf(kgdata,'Koeppen-Geiger-Aug2020.nc','i4',lons,lats,dbg=False)
+##   print('KG ', KG_boreal)
+
+  import os
+  import emxplots.plotmap as plot
+
+  tdir='/lustre/storeB/users/davids/Data_Geo/KoeppenGeiger'
+  if not os.path.exists(tdir):
+    tdir= tdir.replace('storeB','storeA')
+  if 'ppi' not in  os.uname().nodename:
+    tdir = '/home/davids' + tdir
+  assert os.path.exists(tdir),'NO INPUT DIR:'+tdir
+
+  kgb = getKoeppenGeigerBeck(tdir,groupsWanted=[],plotsWanted=False,dbg=False)
+  plot.plotmap(kgb['grid_desert'],'KGB-De')
+
