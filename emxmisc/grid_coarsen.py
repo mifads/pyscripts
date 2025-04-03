26a817432120a446f0b1b1092882a886896b9401
David Simpson <david.simpson@met.no>
2025-04-03 17:01:11 +0200

    Added trim_excess

--- emxmisc/grid_coarsen.py
+++ emxmisc/grid_coarsen.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python3
-import dask as da
+import dask.array as da
 import xarray as xr
 import numpy as np
 import sys
 
 
-def coarsen(x,dx=2,dy=2,f=np.nanmean):
-  """ Uses dask coarsen method to extract coarser grid """
+def coarsen(x,dx=2,dy=2,f=np.nanmean,trim_excess=False):
+  """ Uses dask coarsen method to extract coarser grid
+      CONSIDER trim_excess=True as default, but not sure of
+      implications yet """
   xx = da.array.array(x)
   if xx.ndim == 3: # assumed t,j,i
-    cx = da.array.coarsen(f,xx,{0:1,1:dy,2:dx}) #.compute()
+    cx = da.coarsen(f,xx,{0:1,1:dy,2:dx},trim_excess=trim_excess)
   elif xx.ndim == 2: # assumed e.g. j,i,
-    cx = da.array.coarsen(f,xx,{0:dy,1:dx}) #.compute()
+    cx = da.coarsen(f,xx,{0:dy,1:dx},trim_excess=trim_excess)
   elif xx.ndim == 1: # assumed e.g. lons
-    cx = da.array.coarsen(f,xx,{0:dx}) #.compute()
+    cx = da.coarsen(f,xx,{0:dx},trim_excess=trim_excess)
   return cx.compute()
 
