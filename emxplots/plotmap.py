e1db19777d471ded17b18c0083f1a8f2a551f99a
David Simpson <david.simpson@met.no>
2025-04-11 10:12:02 +0200

    Adding world map with pts

--- emxplots/plotmap.py
+++ emxplots/plotmap.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
    Very simple map plotting, using imshow.
+   or for simple points on nice background map.
    For more advanced, use plotCdfMap.py
 """
 import matplotlib.pyplot as plt
@@ -19,10 +20,41 @@ def plotmap(x,txt,plotfile=None):
     plt.savefig(plotfile)
   plt.clf()
 
+def plot_map_pts(x,y,z,col='r',title='',ofile=None):
+
+  import cartopy.crs as ccrs
+
+  fig = plt.figure(figsize=(10, 4))
+  ax = fig.add_subplot(1, 1, 1, projection=ccrs.PlateCarree())
+
+  # make the map global rather than have it zoom in to
+  # the extents of any plotted data
+  ax.set_global()
+
+  ax.stock_img()
+  ax.coastlines()
+
+  ax.scatter(x,y,z,color=col)
+  if len(title)>0:
+    plt.title(title,fontsize=14)
+
+  plt.tight_layout()
+  if ofile is None:
+    plt.show()
+  else:
+    plt.savefig(ofile)
+  plt.clf()
+
 if __name__ == '__main__':
   x=np.ones([180,360])  # typical emep domain
   for i in range(360):
       x[:,i] = i
 
   plotmap(x,'TestX')
+
+  x = np.linspace(-10,40,10)
+  y = np.linspace(30,60,10)
+  z = np.linspace(0,100,10)
+  plot_map_pts(x,y,z,'r')
+
 
