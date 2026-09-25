80e9e7cbfcdc530e84d6fcc48ff78a748f01901a
David Simpson <david.simpson@met.no>
2026-09-25 10:23:48 +0200

    Added 1d case

--- emxstats/getmax2d.py
+++ emxstats/getmax2d.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 import numpy as np
 
-def getmax2d(array):
+
+def getmax1d(a):
+  """ gets 1st max value in 2-D array """
+  return a.argmax()
+
+def getmax2d(a):
   """ gets 1st max value in 2-D array """
   j, i = np.unravel_index(a.argmax(),a.shape)
   return j,i
+
+if __name__ == '__main__':
+  a= np.array( [1.0, 3.0, 2.5, 6.0, 1.0 ])
+  i = getmax1d(a)
+  print( i, a[i] )
